eec51a1194838fa20be05e43133eb5b482945fc6
Chris Dawalt <cdawalt3dev@gmail.com>
2021-07-31 01:45:29 -0400

    Revert "Update clientinfo.h"
    This reverts commit e858507363885300b298ca3888b1620de9c24b18.

--- src/client/clientinfo.h
+++ src/client/clientinfo.h
@@ -8,7 +8,7 @@
 // WARNING: included before the gamemod's client defs.h file.  Beware if anything
 // important is missing from here (context) because of that.
 
-// abc def
+// abc
 
 typedef struct ClientInfo_s{
 	
