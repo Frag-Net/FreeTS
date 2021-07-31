1847666712932585afd4f6266b505f50b8cba2ba
Chris Dawalt <cdawalt3dev@gmail.com>
2021-07-31 03:00:53 -0400

    test commit 7

--- src/client/clientinfo.h
+++ src/client/clientinfo.h
@@ -8,7 +8,7 @@
 // WARNING: included before the gamemod's client defs.h file.  Beware if anything
 // important is missing from here (context) because of that.
 
-// abc def xyz 123 456 789
+// abc def xyz 123 456 789 aaa
 
 typedef struct ClientInfo_s{
 	
